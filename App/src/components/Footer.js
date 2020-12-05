d7c23bddfc53e5589b253e12d4b4ead602b2cb34
Mohil Palav <mohilpalav@gmail.com>
2020-12-04 21:09:25 -0500

    Footer changed

--- App/src/components/Footer.js
+++ App/src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
   render(){
     return (
       <div className="Footer">
-        <Navbar className="mb-0" bg="dark" variant="dark">
+        <Navbar fixed="bottom" bg="dark" variant="dark">
           <Navbar.Text className="mx-auto">
               Developed by Mohil Palav, Marc Ivan and Aditya Chaudhari.
           </Navbar.Text>
